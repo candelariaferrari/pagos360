f7cb778219a8bb3b25c7ea3b8d54ee3b1a68f70d
Candelaria Ferrari <ferrari.candelaria@gmail.com>
2022-03-15 17:25:20 -0300

    creo componentes

--- ejercicio-pagos360/src/app/app.module.ts
+++ ejercicio-pagos360/src/app/app.module.ts
@@ -4,10 +4,16 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { NavBarComponent } from './components/nav-bar/nav-bar.component';
+import { SearchComponent } from './components/search/search.component';
+import { TableComponent } from './components/table/table.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavBarComponent,
+    SearchComponent,
+    TableComponent
   ],
   imports: [
     BrowserModule,
